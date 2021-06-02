d6e17ddb8b2d10a6815f46cc34fda88c979015d9
myname <mymail>
2021-06-02 09:55:29 +0800

    feat: iceberg and breadcast

--- trader/transmit/src/main/java/com/eis/transmit/service/BroadcastFeignService.java
+++ trader/transmit/src/main/java/com/eis/transmit/service/BroadcastFeignService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(url="${broker-broadcast-service}")
+@FeignClient(url="${broker-broadcast-service}",name = "broker-broadcast-service")
 public interface BroadcastFeignService {
 
     @RequestMapping(value="/broadcast/analysis/getWeight",method = RequestMethod.GET)
